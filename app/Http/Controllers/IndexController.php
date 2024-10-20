1b90e85b5ee486fb30dc54cb666385e0024f4c55
Lilit Karapetyan <karapetyan.lilit.98@gmail.com>
2024-10-20 07:38:31 -0700

    updates

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -39,6 +39,37 @@ class IndexController extends Controller
         // Get seo data
         $seo = Seo::where('page', 'home')->first();
 
+        $data['servicesList'] = [
+            [
+                'icon' => 'Depannage_16152016011.png',
+                'img' => 'Depannage_16152882152.jpg',
+                'title' => 'Boiler Repair and Installation',
+                'description' => 'Ensure your home stays warm and efficient with our expert boiler services. Whether you need a quick repair or a full installation, our certified technicians provide reliable solutions tailored to your needs.',
+                'url' => 'boilers'
+            ],
+            [
+                'icon' => 'Depannage_16152015871.png',
+                'img'  => 'Depannage_16152883352.jpg',
+                'title' => 'Lock Repair and Installation, Locksmith',
+                'description' => 'Locked out or need to upgrade your home’s security? Our locksmiths are ready to assist with fast and secure lock repair and installation services, ensuring your peace of mind.',
+                'url' => 'locksmith'
+            ],
+            [
+                'icon' => 'Depannage_16152015651.png',
+                'img' => 'Depannage_16152884012.png',
+                'title' => 'Electricians',
+                'description' => 'From fixing faulty wiring to installing new lighting, our licensed electricians deliver top-quality electrical services that keep your home safe and functioning smoothly.',
+                'url' => 'electricity'
+            ],
+            [
+                'icon' => 'Depannage_16152015381.png',
+                'img' => 'Depannage_16152884482.jpg',
+                'title' => 'Plumber',
+                'description' => 'Don’t let plumbing issues disrupt your day. Our skilled plumbers are available to tackle everything from leaks to full installations, providing dependable service when you need it most.',
+                'url' => 'plumber'
+            ],
+        ];
+
         // Push data
         $data['slider_items'] = $slider_items;
         $data['about_us'] = $about_us;
--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -18,6 +18,37 @@ class ServicesController extends Controller
 
             $data['seo'] = $seo;
 
+            $data['servicesList'] = [
+                [
+                    'icon' => 'Depannage_16152016011.png',
+                    'img' => 'Depannage_16152882152.jpg',
+                    'title' => 'Boiler Repair and Installation',
+                    'description' => 'Experiencing boiler issues or planning a new installation? Trust our experts to keep your home warm and efficient. Get reliable service today. Click for more information!',
+                    'url' => 'boilers'
+                ],
+                [
+                    'icon' => 'Depannage_16152015871.png',
+                    'img'  => 'Depannage_16152883352.jpg',
+                    'title' => 'Lock Repair and Installation, Locksmith',
+                    'description' => 'Locked out or need to upgrade your home’s security? Our skilled locksmiths provide fast, secure lock repairs and installations. Don’t wait—get the help you need now. Click for more information',
+                    'url' => 'locksmith'
+                ],
+                [
+                    'icon' => 'Depannage_16152015651.png',
+                    'img' => 'Depannage_16152884012.png',
+                    'title' => 'Electricians',
+                    'description' => 'Ensure your home’s electrical system is safe and functioning properly with our expert electricians. From repairs to new installations, we handle it all. Discover dependable service. Click for more information!',
+                    'url' => 'electricity'
+                ],
+                [
+                    'icon' => 'Depannage_16152015381.png',
+                    'img' => 'Depannage_16152884482.jpg',
+                    'title' => 'Plumber',
+                    'description' => 'Dealing with a leak or a plumbing issue? Our experienced plumbers are ready to resolve any problem, big or small. Get prompt, professional service. Click for more information!',
+                    'url' => 'plumber'
+                ],
+            ];
+
             // Senda data to view
             return view('pages.services-list')->with($data);
         }else{
